fa73755c857d9eb2c58de1bb9c772ca327597728
KAI MO <765645110@qq.com>
2017-10-21 15:38:50 +0800

    Update ghost_v1.0.js

--- js/ghost_v1.0.js
+++ js/ghost_v1.0.js
@@ -357,9 +357,9 @@ var dataPackage = {
 	};
 	
     function toDo5Hz(){
-		scratchData[0] = dataPackage.get_flightData;
-		scratchData[1] = 0;
-		//chrome.runtime.sendMessage(googleKey, scratchData, function(){});
+		//scratchData[0] = dataPackage.get_flightData;
+		//scratchData[1] = 0;
+		chrome.runtime.sendMessage(googleKey, scratchData, function(){});
 		console.log(scratchData[0]);
 	}
 
